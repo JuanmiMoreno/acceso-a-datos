0cede3248ab7663aa5c32d3e312a6ae58640abaa
Juan Miguel Moreno Rodriguez <92120520+JuanmiMoreno@users.noreply.github.com>
2022-10-21 13:34:03 +0200

    Insertado busquedad de animal por id

--- ProyectoZoologico/src/prZoologicoDAO/AnimalDAO.java
+++ ProyectoZoologico/src/prZoologicoDAO/AnimalDAO.java
@@ -2,45 +2,85 @@ package prZoologicoDAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public abstract class AnimalDAO {
 
 	private static Connection connection;
-	
-	
-	//BORRAR TODOS LOS ANIMALES
-	public static void deleteAnimal() {
+
+	// BUSCAR UN ANIMAL POR ID
+	public static Animal buscarAnimalID(int id) {
+		Animal animal = null;
 		try {
-		//ABRIR CONEXION
-		connection = openConnection();
-		
-		//SENTENCIA DE BORRAR
-		String query = "delete from animales";
-		Statement statement = connection.createStatement();
-		statement.executeUpdate(query);
-		
+			connection = openConnection();
+			String query = "Select * from Animales where id=?";
+			PreparedStatement ps = connection.prepareStatement(query);
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+				animal = new Animal(rs.getInt("id"), rs.getString("nombre"), rs.getString("habitat"),
+						rs.getFloat("peso_aproximado"));
+			}
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		closeConnection();
-		
+		return animal;
 	}
 
-	
-	
+	// BORRAR ANIMAL DADO SU NOMBRE
+	public static void deleteAnimalNombre(String nombre) {
+		try {
+			connection = openConnection();
+
+			String query = "delete from Animales where nombre=?";
+			PreparedStatement preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setString(1, nombre);
+			preparedStatement.executeUpdate();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		closeConnection();
+
+	}
+
+	// BORRAR TODOS LOS ANIMALES
+	public static void deleteAnimal() {
+		try {
+			// ABRIR CONEXION
+			connection = openConnection();
+
+			// SENTENCIA DE BORRAR
+			String query = "delete from animales";
+			Statement statement = connection.createStatement();
+			statement.executeUpdate(query);
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		closeConnection();
+
+	}
+
 	// FUNCION PARA INSERTAR ATRIBUTOS EN LA TABLA ANIMAL
 	public static void insetAnimal(Animal animal) {
 		try {
-			//ABRE LA CONEXION
+			// ABRE LA CONEXION
 			connection = openConnection();
-			
-			//INSERTAMOS LOS DATOS
+
+			// INSERTAMOS LOS DATOS
 			String query = "insert into animales(nombre,habitat,peso_aproximado) values (?,?,?)";
-			
-			//utilizamos PREPAREDSTATEMENT para meter los valores despues
+
+			// utilizamos PREPAREDSTATEMENT para meter los valores despues
 			PreparedStatement preparedStatement = connection.prepareStatement(query);
 
 			preparedStatement.setString(1, animal.getNombre());
@@ -52,7 +92,7 @@ public abstract class AnimalDAO {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		//CERRAMOS CONEXION
+		// CERRAMOS CONEXION
 		closeConnection();
 
 	}
--- ProyectoZoologico/src/prZoologicoDAO/Main.java
+++ ProyectoZoologico/src/prZoologicoDAO/Main.java
@@ -4,9 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//Animal a = new Animal("Ardilla", "Bosque",(float)0.1);
+		//Animal a = new Animal("Ignacion", "Bosque",(float)0.1);
 		//AnimalDAO.insetAnimal(a);
-		AnimalDAO.deleteAnimal();
+		//AnimalDAO.deleteAnimal();
+		//AnimalDAO.deleteAnimalNombre("Ignacion");
+		Animal a = AnimalDAO.buscarAnimalID(2);
+		System.out.println(a);
 	}
 
 }
